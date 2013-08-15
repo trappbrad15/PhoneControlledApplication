66a5586711ea7f7e838cb50c7b3f46761e514d84
Brad Trapp <bjthdc@gmail.com>
2013-08-14 21:15:48 -0500

    Created Java Project

---
+++ PhoneControlledApplication/PhoneServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PhoneControlledApplication
+{
+    class PhoneServer
+    {
+        private static PhoneServer instance;
+
+        private Socket socket_;
+        private IPAddress ipAddress_;
+        private TcpListener myListener;
+        private bool isRunning_;
+
+        public static PhoneServer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PhoneServer();
+                }
+                return instance;
+            }
+        }
+
+        private PhoneServer()
+        {
+            isRunning_ = true;
+            ipAddress_ = IPAddress.Parse("192.168.0.101");
+            myListener = new TcpListener(ipAddress_, 8001);
+        }
+
+        public void getConnection()
+        {
+            myListener.Start();
+            while (isRunning_)
+            {
+                socket_ = myListener.AcceptSocket();
+                Console.Write(socket_.RemoteEndPoint.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ PhoneControlledApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneControlledApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneServer phoneServer_ = PhoneServer.Instance;
+            phoneServer_.getConnection();
+
+            
+        }
+    }
+}
